00c5b100219639ab9fc45cdae7865d3e3e2d72db
xipper <864353360@qq.com>
2023-03-17 11:48:11 +0800

    connect mysql

--- awsome_chat_gpt_admin/settings.py
+++ awsome_chat_gpt_admin/settings.py
@@ -75,8 +75,16 @@ WSGI_APPLICATION = 'awsome_chat_gpt_admin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'your_database_name',
+        'USER': 'your_mysql_username',
+        'PASSWORD': 'your_mysql_password',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'cursorclass': 'pymysql.cursors.DictCursor',
+        },
     }
 }
 
